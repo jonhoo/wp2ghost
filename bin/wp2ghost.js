4c425ee864695405522a42a104c22904ccd26af8
Jon Gjengset <jon@thesquareplanet.com>
2014-03-23 17:09:53 +0000

    Warn if no filename is given

--- bin/wp2ghost.js
+++ bin/wp2ghost.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 var wp2ghost = require('../lib/wp2ghost.js');
+var path = require('path');
+
+var args = process.argv;
+if (args.length === 1 || (args.length === 2 && args[0].match(/node[\-\.\d]*$/) !== null)) {
+  console.error("Usage: " + path.relative(process.cwd(), __filename) + " wordpress.xml > ghost.json")
+  process.exit();
+}
 
 process.stdout.write("");
 var when = wp2ghost.fromFile(process.argv.slice(-1)[0]);
